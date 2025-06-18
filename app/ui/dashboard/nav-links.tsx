ef9ac1cb437e2cbcb8c3fa057a27dbd67a87ce0f
quangminh.phan <quangminh.phan.se@gmail.com>
2025-06-18 21:32:59 +0700

    build dashboard page

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import {
   UserGroupIcon,
   HomeIcon,
@@ -6,7 +6,7 @@ import {
 } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,7 +22,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-  console.log({pathname})
 
   return (
     <>
@@ -33,10 +32,10 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              }
             )}
           >
             <LinkIcon className="w-6" />
